3e7fd45b13a4382bfad924e7912a3bc997d4afc5
calebe1232 <humcaleb@gmail.com>
2026-07-24 18:48:57 -0300

    Add files via upload

---
+++ Advinhe o numero/java.js
@@ -0,0 +1,10 @@
+let nome = prompt("Qual seu nome?")
+let cidade = prompt("Qual sua cidade?")
+alert("Olá " + nome + "! Você é de " + cidade + ".")
+console.log("Olá " + nome + "! Você é de " + cidade + ".")
+let numero = prompt("Qual seu número secreto?")
+if (numero == 67) {
+    alert("Parabéns! Você acertou o número secreto!")
+} else {
+    alert("Que pena! Tente novamente.")
+}
