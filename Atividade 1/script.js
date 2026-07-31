c9743779a1cc80f7bbbcfaefba8128694b1dc321
calebe1232 <humcaleb@gmail.com>
2026-07-31 18:04:40 -0300

    Add files via upload

---
+++ Atividade 1/script.js
@@ -0,0 +1,12 @@
+let nome = prompt("Digite seu nome completo:");
+let cargo = prompt("Digite seu cargo ou função:");
+let empresa = prompt("Digite o nome da empresa ou escola:");
+let email = prompt("Digite seu e-mail:");
+
+nome = nome.toUpperCase();console.log("      CARTÃO DE VISITA DIGITAL");
+console.log("Nome    : " + nome);
+console.log("Cargo   : " + cargo);
+console.log("Empresa : " + empresa);
+console.log("E-mail  : " + email);
+
+alert("Cartão de visita gerado com sucesso!");
