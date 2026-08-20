7c46d815395645b80632f991d376f619853275ef
calebe1232 <humcaleb@gmail.com>
2026-08-20 17:46:14 -0300

    Add files via upload

---
+++ C15-Aula de hoje/script.js
@@ -0,0 +1,15 @@
+let mensagem;
+if (nota >= 6) {
+    mensagem = 'Aprovado';
+    } else {
+        mensagem = 'Reprovado'
+    }
+
+const mensagem= nota >= 6 ? 'Aprovado' : 'Rreprovado';
+
+let descricao = temperatura >= 30 ? 'Quente' : 'Agradável';
+
+let tipo = numero % 2 === 0 ? 'par' : 'ímpar';
+
+let saudacao = hora =< 12 ? 'Bom dia' : 'Boa tarde/noite';
+
