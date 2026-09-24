d150fc79a71f7f62021b80311534ea62cfc85dde
calebe1232 <humcaleb@gmail.com>
2026-09-24 18:09:13 -0300

    Add files via upload

---
+++ Planejador-Escolar/script.js
@@ -0,0 +1,18 @@
+let tarefas = [];
+
+let totalTarefas = 0;
+let totalConcluídas = 0;
+
+
+function adicionarTarefa() {
+         
+       let nome = document.getElementById("tarefa").value.trim()
+
+       let materia = document.getElementById("materia").value.trim()
+   
+        let prioridade = document.getElementById("prioridade").value;
+        let mensagem = document.getElementById("mensagem");
+
+        if (nome === "")| | materia == 
+
+}
